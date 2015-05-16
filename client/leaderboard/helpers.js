6b04cdf6012268a7d487e8f5b8939fe6638be21a
Sung Won Cho <mikeswcho@gmail.com>
2015-05-16 17:15:35 +1000

    Add sorting

--- client/leaderboard/helpers.js
+++ client/leaderboard/helpers.js
@@ -1,6 +1,6 @@
 Template.leaderboard.helpers({
   player: function () {
-    return PlayersList.find({});
+    return PlayersList.find({}, {sort: {score: -1, name: 1}});
   },
   playersCount: function () {
     return PlayersList.find({}).count();
